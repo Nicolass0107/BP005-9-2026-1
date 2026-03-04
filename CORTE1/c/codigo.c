c4aac62900384a6bfe15ace2125b5f40d33fbdb7
Nicolass0107 <martonte1988@gmail.com>
2026-03-04 12:58:13 -0500

    Practica2

---
+++ CORTE1/c/codigo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, suma;
+
+    printf("Ingrese el primer numero double: ");
+    scanf("%lf", &num1);
+
+    printf("Ingrese el segundo numero double: ");
+    scanf("%lf", &num2);
+
+    
+
+
